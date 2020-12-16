f1d4edad62008a88db110e4fe472735a8c403b9a
malakhovua <malakhovua@gmail.com>
2020-12-16 11:01:34 +0200

    161220

--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,8 @@ Rails.application.routes.draw do
   resources :products
   resources :users
 
-  root 'customer#index', as: 'customer_index'
+  
+  #root 'customer#index', as: 'customer_index'
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
