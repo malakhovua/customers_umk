20b689e2074214eb66608cf79efe57fe34540d36
alex <malakhovua@gmail.com>
2021-11-30 17:23:22 +0200

    301121_2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  resources :roles
   resources :addresses
   resources :exch_nodes
   resources :units
--- db/migrate/20211130150101_create_roles.rb
+++
@@ -1,9 +0,0 @@
-class CreateRoles < ActiveRecord::Migration[5.2]
-  def change
-    create_table :roles do |t|
-      t.belong_to :user
-
-      t.timestamps
-    end
-  end
-end
