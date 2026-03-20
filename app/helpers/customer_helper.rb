8a41084c1620f1a06c5da3b3c3efa8f093721dcb
alex <malakhovua@gmail.com>
2026-03-20 15:08:19 +0200

    modified:   app/helpers/customer_helper.rb

--- app/helpers/customer_helper.rb
+++ app/helpers/customer_helper.rb
@@ -5,8 +5,8 @@ module CustomerHelper
     Pack
       .joins('INNER JOIN products ON packs.product_id = products.unf_id')
       .where(products: { id: product.id })
-      .where(deletion_mark: false, not_active: false)
+      .where.not(deletion_mark: true)
+      .where.not(not_active: true)
       .where.not(id: 0)
   end
-
 end
